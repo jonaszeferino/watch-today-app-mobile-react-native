4f8dfd5cbd17839b987b2b33cc6731ac9c0c7466
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-04-16 17:35:22 -0300

    secound commit woks with problens

--- App.js
+++ App.js
@@ -1,35 +1,40 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Image, Button, StyleSheet, Text, View } from "react-native";
 import { useEffect, useState } from "react";
+import callbackImage from "./assets/callback.png";
 
 export default function App() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
   const apiCall = () => {
-    const url = `https://api.themoviedb.org/3/movie/123?api_key=dd10bb2fbc12dfb629a0cbaa3f47810c&language=pt-BR`;
+    const randomId = Math.floor(Math.random() * 560000) + 1;
+    const url = `https://api.themoviedb.org/3/movie/${randomId}?api_key=dd10bb2fbc12dfb629a0cbaa3f47810c&language=pt-BR`;
 
     fetch(url)
       .then((response) => response.json())
       .then((result) => {
         setData({
-          budget: result.budget,
-          originalTitle: result.original_title,
-          portugueseTitle: result.title,
-          overview: result.overview,
-          average: result.vote_average,
-          releaseDate: result.release_date,
-          image: result.poster_path,
-          country: result.production_countries[0].name,
-          ratingCount: result.vote_count,
-          popularity: result.popularity,
-          gender: result.genres.map((genre) => genre.name),
-          languages: result.spoken_languages[0].name,
-          adult: result.adult,
-          movieId: result.id,
-          originalLanguage: result.original_language,
-          statusMovie: result.status,
+          budget: result.budget || null,
+          originalTitle: result.original_title || null,
+          portugueseTitle: result.title || null,
+          overview: result.overview || null,
+          average: result.vote_average || null,
+          releaseDate: result.release_date || null,
+          image: result.poster_path || null,
+
+          ratingCount: result.vote_count || null,
+          popularity: result.popularity || null,
+          gender:
+            (result.genres && result.genres.map((genre) => genre.name)) || null,
+          languages:
+            (result.spoken_languages && result.spoken_languages[0].name) ||
+            null,
+          adult: result.adult || null,
+          movieId: result.id || null,
+          originalLanguage: result.original_language || null,
+          statusMovie: result.status || null,
         });
         setIsLoading(false);
       })
@@ -40,9 +45,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World! 4</Text>
-      <Text>{data && data.originalTitle}</Text>
+      <Text>{data.originalTitle}</Text>
       <Text>{data && data.budget}</Text>
-      <Text>{data && data.releaseDate}</Text>
+      <Image
+        source={
+          data && data.image
+            ? { uri: "https://image.tmdb.org/t/p/original" + data.image }
+            : callbackImage
+        }
+      />
       <StatusBar style="auto" />
       <Button
         title="Verificar"
