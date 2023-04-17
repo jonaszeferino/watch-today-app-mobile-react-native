1cfa874f61ee7679aef57d602f7bb5732a6e5517
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-04-16 22:30:26 -0300

    what to watch today page almost done

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { Image, Button, StyleSheet, Text, View } from "react-native";
+import { Header } from "react-native-elements";
 import { useEffect, useState } from "react";
 import callbackImage from "./assets/callback.png";
 
 export default function App() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [showCallbackImage, setShowCallbackImage] = useState(true);
 
   const apiCall = () => {
+    setIsLoading(true);
+    setIsError(false);
+
     const randomId = Math.floor(Math.random() * 560000) + 1;
     const url = `https://api.themoviedb.org/3/movie/${randomId}?api_key=dd10bb2fbc12dfb629a0cbaa3f47810c&language=pt-BR`;
 
@@ -23,7 +29,6 @@ export default function App() {
           average: result.vote_average || null,
           releaseDate: result.release_date || null,
           image: result.poster_path || null,
-
           ratingCount: result.vote_count || null,
           popularity: result.popularity || null,
           gender:
@@ -37,31 +42,91 @@ export default function App() {
           statusMovie: result.status || null,
         });
         setIsLoading(false);
+        setShowCallbackImage(false);
       })
-      .catch((error) => console.error(error));
+      .catch((error) => {
+        console.error(error);
+        setIsError(true);
+        setShowCallbackImage(true);
+      });
   };
   console.log(data);
 
+  const imageUrl = data?.image
+    ? `https://image.tmdb.org/t/p/original${data.image}`
+    : callbackImage;
+
   return (
     <View style={styles.container}>
-      <Text>Hello World! 4</Text>
-      <Text>{data.originalTitle}</Text>
-      <Text>{data && data.budget}</Text>
-      <Image
-        source={
-          data && data.image
-            ? { uri: "https://image.tmdb.org/t/p/original" + data.image }
-            : callbackImage
-        }
+      <Header
+        backgroundColor="#3D6DCC"
+        leftComponent={{ icon: "menu", color: "#fff" }}
+        centerComponent={{
+          text: "What to watch today?",
+          style: { color: "#fff" },
+        }}
+        rightComponent={{ icon: "home", color: "#fff" }}
       />
+      {(isError || (!isLoading && !data)) && (
+        <View style={styles.callbackContainer}>
+          <Image source={callbackImage} style={styles.callbackImage} />
+          {isError && (
+            <Text style={styles.errorText}>
+              Ocorreu um erro ao carregar os dados
+            </Text>
+          )}
+        </View>
+      )}
+      {data && (
+        <>
+          <View style={styles.table}>
+            <View style={styles.row}>
+              <Text style={styles.header}>Title </Text>
+              <Text>{data.originalTitle}</Text>
+            </View>
+          </View>
+          <Image
+            style={{ height: 504, width: 336 }}
+            source={{ uri: imageUrl }}
+          />
+          {data.popularity && (
+            <View style={styles.row}>
+              <Text style={styles.header}>Popularity </Text>
+              <Text>{data.popularity}</Text>
+            </View>
+          )}
+          {data.ratingCount && (
+            <View style={styles.row}>
+              <Text style={styles.header}>Rating </Text>
+              <Text>{data.ratingCount}</Text>
+            </View>
+          )}
+          {data.budget && (
+            <View style={styles.row}>
+              <Text style={styles.header}>Budget </Text>
+              <Text>{data.budget}</Text>
+            </View>
+          )}
+        </>
+      )}
       <StatusBar style="auto" />
-      <Button
-        title="Verificar"
-        size="lg"
-        colorScheme="purple"
-        mt="24px"
-        onPress={apiCall}
-      />
+      {data || isError ? (
+        <Button
+          title="Novo Filme"
+          size="lg"
+          colorScheme="purple"
+          mt="24px"
+          onPress={apiCall}
+        />
+      ) : (
+        <Button
+          title="Verificar"
+          size="lg"
+          colorScheme="purple"
+          mt="24px"
+          onPress={apiCall}
+        />
+      )}
     </View>
   );
 }
@@ -72,5 +137,20 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  table: {
+    marginVertical: 20,
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  header: {
+    fontWeight: "bold",
   },
 });
---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  images: {
+    domains: [
+      "image.tmdb.org",
+      "https://image.tmdb.org/t/p/original",
+      "image.tmdb.org/t/p/original",
+    ],
+  },
+
+  experimental: {
+    forceSwcTransforms: true,
+    newNextLinkBehavior: false,
+  },
+};
